96b23579e11a6c9849ecddd2de37446537e76b44
Kishan Wanigasinghe <kwanigasinghe@quicksolutions.com>
2017-03-07 14:01:51 -0500

    webpack test project init upload

---
+++ webpack_stuff/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	entry: "./entry.js",
+	output: {
+		path: __dirname,
+		filename: "bundle.js"
+	},
+	module:{
+		loaders: [
+			{test: /\.css$/, loader: "style-loader!css-loader"}
+		]
+	},
+	watch: true,
+	stats:{
+		colors: true
+	}
+};
